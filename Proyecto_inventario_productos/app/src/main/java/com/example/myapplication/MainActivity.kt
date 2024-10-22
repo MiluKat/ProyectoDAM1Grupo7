eb68c8447810dbb5aa5621a770fa57528b02606a
katerina garay <katerina.garay@gmail.com>
2024-10-22 18:22:48 -0500

    Login proyecto

---
+++ Proyecto_inventario_productos/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+        private lateinit var binding: ActivityMainBinding
+        override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+            if(UtilsSharedPreferences.getSesion(this)){
+                startActivity(Intent(this,UserRoom ::class.java).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK))
+            }else {
+                binding = ActivityMainBinding.inflate(layoutInflater)
+                setContentView(binding.root)
+                ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                    val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                    v.setPadding(
+                        systemBars.left,
+                        systemBars.top,
+                        systemBars.right,
+                        systemBars.bottom
+                    )
+                    insets
+                }
+                binding.btnLogin.setOnClickListener {
+                    if (binding.etEmail.text.toString().trim() == "lucas" && binding.etPassword.text.toString().trim() == "1234") {
+                        UtilsSharedPreferences.createSesion(this)
+                        startActivity(Intent(this,UserRoom ::class.java).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK))
+                    } else {
+                        Toast.makeText(this, "Error en las credenciales", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+       }
+    }
+}
---
+++ Proyecto_inventario_productos/app/src/main/java/com/example/myapplication/UtilsSharedPreferences.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+
+class UtilsSharedPreferences {
+    companion object{
+        fun createSesion(context: Context){
+            val sharedPref = context.getSharedPreferences("com.midominio.miaplicacion", MODE_PRIVATE)
+            val editor = sharedPref.edit()
+            editor.putBoolean("login",true)
+            editor.apply()
+        }
+
+        fun getSesion(context: Context):Boolean{
+            val sharedPref = context.getSharedPreferences("com.midominio.miaplicacion", MODE_PRIVATE)
+            return sharedPref.getBoolean("login", false)
+        }
+
+        fun cerrarSesion(context: Context){
+            val sharedPref = context.getSharedPreferences("com.midominio.miaplicacion", MODE_PRIVATE)
+            val editor = sharedPref.edit()
+            editor.putBoolean("login", false)
+            editor.apply()
+        }
+    }
+}
